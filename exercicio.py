354ca4124b8a723498fa859c3d90f1b8949321ed
alexiatai <47820420+alexiatai@users.noreply.github.com>
2019-04-10 10:30:32 -0300

    Update exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -1,3 +1,4 @@
+
 print("Entre com a opção de sua preferência")
 opcao = int(input("1- Exercício 1, 2- Exercício 2, 3- Exercício 3, 4- Exercício 4, 5- Exercício 5, 6- Exercício 6, 7- Exercício 7, 8- Exercício 8, 9- Exercício 9, 10- Exercício 10"))
 if opcao == 1:
@@ -46,7 +47,25 @@ if opcao == 6:
   novo_b = a
   return novo_a, novo_b
 
+if opcao == 8:
+
+    def calcular_equacao(valor_x, valor_a, valor_b, valor_c):
+        equacao = valor_a * (valor_x**2) + valor_b * valor_x + valor_c 
+            return equacao
+
 if opcao == 9: 
 
     def converter_celsius_fahrenheit(celsius):
+        fahrenheit = 9.0/5.0 * celsius + 32
+        return fahrenheit
 
+if opcao == 10:
+def calcular_media(nota1, nota2, nota3):
+    media = (nota1*2) + (nota2*3) + (nota3*5)
+    ponderada = media/(2+3+5)
+    return ponderada 
+
+calcular_media = calcular_media(5, 3, 4)
+print(calcular_media)
+
+"""Mp = 7,0·1 + 6,0·2 + 8,0·3 + 7,5·4/1 + 2 + 3 + 4"""
