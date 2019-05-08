4e643b62d298680cf85243a1abaa3844f39e2c0f
alexiatai <47820420+alexiatai@users.noreply.github.com>
2019-05-07 21:04:59 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -56,16 +56,11 @@ def calcular_media(prova1, prova2, prova3):
         return "Em recuperação", media
 
 
-def criar_triangulo(numero):
+def imprimir_figura(numero):
   lista_de_numero = []
-  if numero <= 0 :
-    exit("Insira um nº maior que zero")
-  for linha in range(1, numero+1):
-    lista_de_numero.append("*" *linha)
+  for linha in range (1,numero+1):
+    lista_de_numero.append(linha * "*")
   return lista_de_numero
-  
-numero = int(input("Insira o nº de linhas: "))
-lista = criar_triangulo(numero)
 
 def calcular_par_impar(numero):
     if numero%2 == 0:
@@ -75,18 +70,6 @@ def calcular_par_impar(numero):
 
     
 """FALTA A SEIS"""
-
-def imprimir_figura(numero):
-  lista_de_numero = []
-  for linha in range (1,numero+1):
-    lista_de_numero.append(linha * "*")
-  return lista_de_numero
-  
-numero = int(input("Insira as linhas: "))
-lista = imprimir_figura(numero)
-for linha in lista:
-  print(linha)
-
 
 
 def adivinhar_valor(numero):
@@ -100,6 +83,10 @@ def adivinhar_valor(numero):
     if numero < valor:
         return "O valor é maior, tente outra vez"
 
+
+"""FALTA A OITO"""
+
+
 def ordenar_valores(valores):
     ordenar = valores
     numeros_ordenados = sorted(ordenar)
@@ -107,19 +94,86 @@ def ordenar_valores(valores):
     
  """FALTA A DEZ """
    
-
 if __name__=="__main__":
 
     print("Opções de funções")
-    print("1- Receber 3 inteiros e devolver o menor deles.")
-    print("2- Verificar se  ́e um triângulo equilátero, isóceles ou escaleno.")
-    print("3- Situação e média de um  aluno.")
-    print("4- Calcular gasto de gasolina")
-    print("5- Imprimir nome e qte de letras")
-    print("6- Calcular salário do vendedor")
-    print("7- Trocar valor de variáveis")
-    print("8- Equação do segundo grau")
-    print("9- Converter graus em fahrenheit")
-    print("10- Média ponderada")
+    print("1-Determinar o menor dos inteiros")
+    print("2- Verificar tipo do triângulo")
+    print("3- Média de três notas")
+    print("4- Verificar par ou ímpar")
+    print("5- Imprimir figura")
+    print("6- Calcular expressão")
+    print("7- Jogo de adivinhar o número")
+    print("8- Ordenar valores")
+    print("9- Números em ordem crescente")
+    print("10- Procurar informação na lista ")
 
-opcao = int(input("Insira a opção: "))
+    opcao = int(input("Insira a opção: "))
+
+    if opcao == 1:
+
+        num1 = float(input("Número 1: "))
+        num2 = float(input("Número 2: "))
+        num3 = float(input("Número 3: "))
+        menor_int = descobrir_menor_inteiro(num1, num2, num3)
+        print(menor)
+
+
+    if opcao == 2 :
+
+        valor_x = float(input("Valor de X: "))
+        valor_y = float(input("Valor de Y: "))
+        valor_z = float(input("Valor de Z: "))
+        triangulo = verificar_tipo_triangulo(valor_x, valor_y, valor_z)       
+        print(triangulo)
+
+
+    if opcao == 3: 
+
+        prova1 = float(input("Nota prova 1: "))
+        prova2 = float(input("Nota prova 2: "))
+        prova3 = float(input("Nota prova 3: "))
+        nota = calcular_media(prova1, prova2, prova3)
+        print(nota)
+
+
+    if opcao == 4: 
+
+        numero  = float(input("Número de linhas: "))
+        triangulo = imprimir_figura(numero)
+        print(triangulo)
+
+
+    if opcao == 5:
+
+        numero  = input("Valor para descobrir se é ímpar ou par: ")
+        valor = calcular_par_impar(numero)
+        print(valor)
+
+
+    if opcao == 6:
+
+        print("Em manutenção")
+
+
+    if opcao == 7:
+
+        numero  = input("Valor para tentar adivinhar: ")
+        adivinhar = adivinhar_valor(numero)
+        print(adivinhar)
+
+
+    if opcao == 8:
+
+        print("Em manutenção")
+
+    if opcao == 9: 
+
+        valores = float(input("Entre com os valores que irão ser ordenados: "))
+        ordenar = ordenar_valores(valores)
+        print(ordenar)
+
+
+    if opcao == 10:
+
+        print("Em manutenção")
