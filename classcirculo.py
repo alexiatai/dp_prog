356ebe47381a9e5e68091b7d87f45f0d530cd544
alexiatai <47820420+alexiatai@users.noreply.github.com>
2019-08-27 15:59:45 -0300

    Update classcirculo.py

--- classcirculo.py
+++ classcirculo.py
@@ -1,14 +1,15 @@
 class Circulo:
 
-    def __init__(self, raio):
+    def __init__(self, raio, x, y):
         self.raio = raio
+        self.centro = (x, y)
 
     def verificar_raio(self,raio):
         if self.raio<=0:
             raio = 1
 
     def calcular_area(self):
-        return 3,14*raio**2
+        return 3.14*self.raio**2
          
 
 raio = float(input("Entre com um valor para raio: "))
